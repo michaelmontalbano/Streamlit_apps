b73d11839e1c4072e91bc18558b6133234fe9802
michaelmontalbano <michael.montalbano@ou.edu>
2021-11-30 09:40:06 -0600

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -12,6 +12,13 @@ loss_functions = ['mean_squared_error', 'mean_absolute_error']
 
 loss = st.radio("Pick a loss function", loss_functions)
 
+y_test = np.load('/data/y_test_raw.npy')
+y_pred = np.load('/data/y_pred_raw.npy')
+
+st.text_input("Pick a sample number (0-939)", key="sample")
+
+sample = int(st.session_state.sample)
+
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
