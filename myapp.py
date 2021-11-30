dd40d1167b4db9c6070abf8f6195d7f270620310
michaelmontalbano <michael.montalbano@ou.edu>
2021-11-30 09:59:21 -0600

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -9,17 +9,24 @@ st.write("""
 Choose a loss function.
 """)
 
-loss_functions = ['mean_squared_error', 'mean_absolute_error']
+loss_functions = ['mse', 'mae']
 
 loss = st.radio("Pick a loss function", loss_functions)
 
 y_test = load('data/y_test_raw.npy')
-y_pred = load('data/y_pred_raw.npy')
+
+if loss == 'mse':
+    y_pred = load('data/y_pred_raw.npy')
+else:
+    y_pred = load('data/y_pred_{}.npy'.format(loss))
 
 number = st.slider("Pick a sample number (0-939)",0,939)
 
 y_true = np.squeeze(y_test[number])
 y_true = np.interp(y_true, (y_true.min(), y_true.max()), (0, +1))
 
-st.image(y_true,width=200)
+y_pred = np.squeeze(y_pred[number])
+y_pred = np.interp(y_pred, (y_pred.min(), y_pred.max()), (0, +1))
 
+st.image(y_true,width=600)
+st
