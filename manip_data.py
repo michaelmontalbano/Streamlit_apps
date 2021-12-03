06948ffbf26d080e8a24aea6e8b33b6bf0999e93
michaelmontalbano <michael.montalbano@ou.edu>
2021-12-03 16:13:43 -0600

    puushing

--- manip_data.py
+++ manip_data.py
@@ -9,18 +9,19 @@ y_pred_mse_app = []
 y_pred_mae_app = []
 
 y_true = np.load('data/y_true.npy')
-y_pred_mse = np.load('data/y_pred_MSE_best.npy')
+y_pred_mse = np.load('data/y_pred_raw.npy')
 y_pred_mae = np.load('data/y_pred_mae_full.npy')
-
-# print(y_pred_mae.shape)
-# sys.exit()
 
 
 for i in np.arange(0,939,1):
+
     y_true_binary = np.where(y_true[i]<26,0,1)
+
     y_pred_binary = np.where(y_pred_mse[i]<26,0,1)
+    
     if np.sum(y_true_binary) >= thres and np.sum(y_pred_binary)>= thres:
         print(np.sum(y_true_binary))
+        print(np.sum(y_pred_binary))
         y_true_app.append(y_true[i])
         y_pred_mse_app.append(y_pred_mse[i])
         y_pred_mae_app.append(y_pred_mae[i])
